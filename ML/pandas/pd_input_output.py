aacc0b5e25d6fecd7e88e04a746b15c586ef2b0d
pratapsgit <pk1979m@gmail.com>
2021-12-28 10:51:24 +0530

    Added the input output section along with the pandas excercise section

--- ML/pandas/pd_input_output.py
+++ ML/pandas/pd_input_output.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sqlalchemy import create_engine
 
 
 def operations():
@@ -9,6 +10,17 @@ def operations():
     data.to_csv('my_output',index=False)
     df = pd.read_csv('/home/pratap/MyWorks/python_prog/python_practice/ML/pandas/my_output')
     print(df)
+
+    pr_data = pd.read_html('https://stats.espncricinfo.com/ci/content/records/223646.html')
+    print(pr_data)
+    
+    engine = create_engine('sqlite:///:memory:')
+
+    df.to_sql('my_table', engine)
+
+    sqldf = pd.read_sql('my_table', con=engine)
+
+    print(sqldf)
 
 if __name__ == "__main__":
     operations()
---
+++ ML/pandas/pd_sf_salaries.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+def operations():
+    # Read Salaies.csv as a dataframe called sal
+
+    # check the head of the dataframe
+
+    # Use the .info() method to find out how many entries there are
+
+    # What is the average base pay?
+
+    # What is the highest amount of OvertimePay in the dataframe?
+    
+    #  What is the job title of JOSEPH DRISCOLL ?
+
+    # How much does JOSEPH DRISCOLL make (including benifits)?
+
+    # What is the name of the highest paid person (including benifits)?
+
+    # What is the name of the lowest paid person (including benifits)?
+
+    # What was the average(mean) BasePay of all employees per year(2011-2014)?
+
+    # How may unique job titles are there?
+
+    # Waht are the top 5 most common jobs ?
+
+    # How many job Titles were represented by only one person in year 2013 ?
+
+    # How many people have the word Chief in their job title?
+
+    # Is there a correlation between length of the job title string and Salary?
+    
+    pass
+
+if __name__ == "__main__":
+    operations()
